876978a17d4486c13127e8884cf54376efa59bd4
Emile Nijssen <emilenijssen@gmail.com>
2015-04-13 21:48:44 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,28 @@
+"use strict";
+  
+function App() 
+{
+	
+}
+
+module.exports = App;
+
+App.prototype.init = function(){
+	
+	console.log('init');
+	
+	Homey.manager('ledring').animate({
+		name: 'pulse'
+	});
+};
+
+App.prototype.events = {
+	speech: function( speech ) {
+		Homey.say( __("hallo") );
+	}
+};
+
+function foo() {}
+
+while (1)
+  foo();
